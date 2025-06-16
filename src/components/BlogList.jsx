eecb957139744f1cda976266ed68577290d482e3
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-16 22:02:56 +0530

    complete header section

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { blogCategories } from '../assets/assets'
+
+function BlogList() {
+  return (
+    <div>
+        <div className='flex justify-center gap-4 sm:gap-8 my-10 relative'>
+          
+          {blogCategories.map((item)=>(
+            <div key={item} className='relative'>
+                <button>
+                    {item}
+                </button>
+
+            </div>
+          ))}
+         
+        </div>
+        <div>
+            {/* ----blog cards--- */}
+        </div>
+    </div>
+  )
+}
+
+export default BlogList
