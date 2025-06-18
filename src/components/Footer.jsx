903456d097aff8823cb8f6e3992dde53cfa88469
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-18 15:39:04 +0530

    the blog page started

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import { assets, footer_data } from '../assets/assets'
+import React from "react";
+import { assets, footer_data } from "../assets/assets";
 
 function Footer() {
   return (
-    <div className='px-6 md:px-16 lg:px:24 xl:px-32 bg-purple-600/10'>
-        <div>
-            <div className='flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b brorder-gray-500/30 text-gray-500'>
-            <div>
-                <img src={assets.logo} alt='logo' className='w-32 sm:w-44' />
-                <p className='max-w-[410px] mt-6'>Welcome to Quick Blog App — your go-to space for sharing thoughts, exploring ideas, and connecting with readers from around the world.</p>
-            </div>
-
-            </div>
-            <div className='flex flex-wrap justify-between w-fukk md:w[45%] gap-5'>
-                {footer_data.map((section,index)=>(
-                    <div key={index}>
-                       <h3 className='font-semibold text-base text-gray-900 md:mb-5 mb-2'>{section.title}</h3>
-                       <ul className='text-sm space-y-1'>
-                        {section.links.map((link,i)=>(
-                            <li key={i}>
-                                 <a href='#' className='hover:underline transition'>{link}</a>
-                            </li>
-                        ))}
-                       </ul>
-                    </div>
-                ))}
-            </div>
-
+    <div className="px-6 md:px-16 lg:px:24 xl:px-32 bg-purple-600/10">
+      <div>
+        <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b brorder-gray-500/30 text-gray-500">
+          <div>
+            <img src={assets.logo} alt="logo" className="w-32 sm:w-44" />
+            <p className="max-w-[410px] mt-6">
+              Welcome to Quick Blog App — your go-to space for sharing thoughts,
+              exploring ideas, and connecting with readers from around the
+              world.
+            </p>
+          </div>
         </div>
-        <p className='py-4 text-center text-sm md:text-base text-gray-500/80'>Copyright 2025 @ QuickBlog Sanitta - All right Reserved</p>
-      
+        <div className="flex flex-wrap justify-between w-fukk md:w[45%] gap-5">
+          {footer_data.map((section, index) => (
+            <div key={index}>
+              <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">
+                {section.title}
+              </h3>
+              <ul className="text-sm space-y-1 text-gray-500">
+                {section.links.map((link, i) => (
+                  <li key={i}>
+                    <a href="#" className="hover:underline transition">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+        Copyright 2025 @ QuickBlog Sanitta - All right Reserved
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { assets, blog_data, comments_data } from "../assets/assets";
+import Navbar from "../components/Navbar";
+import Moment from "moment";
 
 function Blog() {
-  return (
-    <div>
-      <h1>blog</h1>
+  const { id } = useParams();
+
+  const [data, setData] = useState(null);
+  const [comments, setComments] = useState(null);
+  const fetchBlogData = async () => {
+    const data = blog_data.find((item) => item._id === id);
+    setData(data);
+  };
+
+
+  const fetchComments = async()=>{
+    setComments(comments_data)
+  }
+
+
+
+  useEffect(() => {
+    fetchBlogData();
+    fetchComments()
+  }, []);
+
+  return data ? (
+    <div className="relative">
+      <img
+        src={assets.gradientBackground}
+        className="absolute -top-50 -z-1 opacity-50"
+      />
+      <Navbar />
+      <div className="text-center mt-20 text-gray-600">
+        <p className="text-purple-600 py-4 font-medium">
+          Published on {Moment(data.createdAt).format("MMMM Do YYYY")}
+        </p>
+        <h1 className="text-2xl sm:text-5xl font-semibold max-w-2xl mx-auto text-gray-800">
+          {data.title}
+        </h1>
+        <h2 className="my-5 max-w-lg truncate mx-auto">{data.subTitle}</h2>
+        <p className="inline-block py-1 px-4 rounded-full mb-6 border text-sm border-purple-600 bg-purple-600/10 font-medium text-primary">
+          Sanitta Johnson
+        </p>
+      </div>
+      <div className="mx-5 max-w-5xl md:mx-auto my-10 mt-6">
+        <img src={data.image} alt="" className="rounded-3xl mb-5" />
+        <div
+          className="rich-text max-w-3xl mx-auto"
+          dangerouslySetInnerHTML={{ __html: data.description }}
+        ></div>
+        {/* {comments section} */}
+        <div className="mt-14 mb-10 max-w-3xl mx-auto">
+          <p className="font-semibold mb-4">Comment ({comments.length})</p>
+
+        </div>
+      </div>
     </div>
-  )
+  ) : (
+    <div>Loding....</div>
+  );
 }
 
-export default Blog
+export default Blog;
