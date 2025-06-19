a33bc1c1df53ed88c761189014121ef9c486055c
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-19 23:08:52 +0530

    the admin pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
-import Blog from './pages/Blog'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Blog from "./pages/Blog";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/AddBlog";
+import ListBlog from "./pages/admin/ListBlog";
+import Comments from "./pages/admin/Comments";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/blog/:id' element={<Blog/>}></Route>
- 
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/blog/:id" element={<Blog />}></Route>
+        <Route path="admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
