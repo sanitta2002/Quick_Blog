7e0b5ae176a2020ebd9e2270255db6405086ade3
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-23 22:50:48 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/blog/:id" element={<Blog />}></Route>
         <Route path="admin/login" element={<Login/>}/>
-        <Route path="admin" element={false ? <Layout /> : <Login/>}>
+        <Route path="admin" element={true ? <Layout /> : <Login/>}>
           <Route index element={<Dashboard />} />
           <Route path="addBlog" element={<AddBlog />} />
           <Route path="listBlog" element={<ListBlog />} />
