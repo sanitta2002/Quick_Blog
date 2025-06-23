ae3559b647847b4f429b4cd2f76641bc34bfebbd
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-23 23:05:35 +0530

    dashbord making

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { assets, dashboard_data } from '../../assets/assets'
+import { useSpring } from 'motion/react'
+import { useState } from 'react'
 
 function Dashboard() {
+  const [dashboardData , setDashboardData] = useState({
+    blogs:0,
+    comments:0,
+    dreaft:0,
+    recentBlogs:[]
+  })
+  const fetchDashboard = async ()=>{
+    setDashboardData(dashboard_data)
+  }
+  useEffect(()=>{
+   fetchDashboard()
+  },[])
   return (
-    <div>
-      
+    <div className='flex-1 p-4 md:p-10 bg-blue-50/50'>
+      <div className='flex flex-wrap gap-4'>
+        <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+          <img src={assets.dashboard_icon_1} alt="" />
+          <div>
+            <p className='text-xl font-semibold text-gray-600'>{dashboardData.blogs}</p>
+            <p className='text-gray-400 font-light'>Blogs</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
