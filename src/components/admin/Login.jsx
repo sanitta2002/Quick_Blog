58da5163cc42e8f01aa86cc19ed4ba2f21f6ff5e
sanitta johnson <sanittajohnson28@gmail.com>
2025-06-24 22:51:48 +0530

    login page style done

--- src/components/admin/Login.jsx
+++ src/components/admin/Login.jsx
@@ -11,8 +11,8 @@ function Login() {
 
   return (
     <div className='flex items-center justify-center h-screen'>
-      <div className='w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg'>
-        <div className='flex flex-col items-center justify-center'>
+      <div className='w-full max-w-sm p-6 max-md:m-6 border border-purple-600/30 shadow-2xl shadow-primary/15 rounded-lg bg-purple-600/20'>
+        <div className='flex flex-col items-center justify-center '>
             <div className='w-full py-6 text-center'>
               <h1 className='text-3xl font-bold'><span className='text-primary'>Admin Login</span></h1>
               <p className='font-light'>Enter your credentials to access the admin panel</p>
@@ -20,11 +20,11 @@ function Login() {
             <form onSubmit={handleSubmit} action="" className='mt-6 w-full sm:max-w-md text-gray-600'>
               <div className='flex flex-col'>
                 <label>Email</label>
-                <input onChange={(e)=>setEmail(e.target.value)} value={email} type='email' required placeholder='Your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6'/>
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} type='email' required placeholder='Your email id' className='border-b-2 border-primary/40 p-2 outline-none mb-6'/>
               </div>
               <div className='flex flex-col'>
                 <label>Password</label>
-                <input onChange={(e)=>setPassword(e.target.value)} value={password} type='password' required placeholder='Enter your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6'/>
+                <input onChange={(e)=>setPassword(e.target.value)} value={password} type='password' required placeholder='Enter your password' className='border-b-2 border-primary/40 p-2 outline-none mb-6'/>
               </div>
               <button type='submit' className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all'>Login</button>
             </form>
